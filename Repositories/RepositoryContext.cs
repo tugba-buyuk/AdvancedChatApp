46d77e3cc26986df91583eefbda3df00c9aafd12
tugba-buyuk <tugba.buyuk@std.yildiz.edu.tr>
2024-08-23 20:35:40 +0300

    Initial commit

---
+++ Repositories/RepositoryContext.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class RepositoryContext : IdentityDbContext<User>
+    {
+        public RepositoryContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder.ApplyConfiguration(new ProductConfig());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ SignalRApp/Extensions/ServiceExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repositories;
+
+namespace SignalRApp.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureSqlContext(this IServiceCollection services,
+            IConfiguration configuration) =>
+                services.AddDbContext<RepositoryContext>(options =>
+                options.UseSqlServer(
+                 configuration.GetConnectionString("sqlConnection"),
+                  b => b.MigrationsAssembly("Repositories")));
+    }
+}
